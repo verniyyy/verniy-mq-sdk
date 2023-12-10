2ec4ee7a77a80b80e53234a235fe1b70b88b481f
verniyyy <orekkueito2811@gmail.com>
2023-12-11 04:00:57 +0900

    ✅ example test

--- vmq_example_test.go
+++ vmq_example_test.go
@@ -2,25 +2,107 @@ package vmq
 
 import "log"
 
+// SessionOptionsExample ...
+var SessionOptionsExample = &Options{
+	Addr:     "localhost:9000",
+	UserID:   "01HG17X22440GTQW3AS6WHCF0K",
+	Password: "P@ssw0rd",
+}
+
 func ExampleSession() {
-	const addr = "localhost:9000"
-	sess, err := NewSession(&Options{
-		Addr:     addr,
-		UserID:   "",
-		Password: "",
-	})
+	sess, err := NewSession(SessionOptionsExample)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 	defer func() {
-		log.Println("Close session")
+		if err := sess.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	//Output:
+	//
+}
+
+func ExamplePing() {
+	sess, err := NewSession(SessionOptionsExample)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer func() {
 		if err := sess.Close(); err != nil {
 			panic(err)
 		}
 	}()
 
 	if err := Ping(sess); err != nil {
-		log.Fatal(err)
+		log.Println(err)
+	}
+
+	//Output:
+	//
+}
+
+// ExampleCreateQueue ...
+func ExampleCreateQueue() {
+	sess, err := NewSession(SessionOptionsExample)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer func() {
+		if err := sess.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := CreateQueue(sess, "example queue"); err != nil {
+		log.Println(err)
+	}
+
+	//Output:
+	//
+}
+
+// ExampleDeleteQueue ...
+func ExampleDeleteQueue() {
+	sess, err := NewSession(SessionOptionsExample)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer func() {
+		if err := sess.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := DeleteQueue(sess, "example queue"); err != nil {
+		log.Println(err)
+	}
+
+	//Output:
+	//
+}
+
+// ExamplePublish ...
+func ExamplePublish() {
+	sess, err := NewSession(SessionOptionsExample)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer func() {
+		if err := sess.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	msg := User{ID: 1, Name: "Jhon"}
+
+	if err := Publish(sess, "example queue", msg); err != nil {
+		log.Println(err)
 	}
 
 	//Output:
